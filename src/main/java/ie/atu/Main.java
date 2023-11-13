140dcc032bcedd089659e5b07d8be383bc6d95bf
Tremain <g00367973@atu.ie>
2023-11-13 09:47:21 +0000

    Example using try catch to enter a number between 0 and 9

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,34 @@
+package ie.atu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+
+        try
+        {
+            System.out.println("Enter number between 0 - 9: ");
+            Scanner myscan = new Scanner(System.in);
+
+            String input = myscan.nextLine();
+
+            int myNum = Integer.parseInt(input);
+
+            if (myNum >= 0 && myNum <= 9)
+            {
+                System.out.println("You entered " + myNum);
+            }
+
+            else
+            {
+                System.out.println("Not a valid number");
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("Enter a valid numeric value.");
+        }
+    }
+}
